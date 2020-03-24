28bd378b3580876e63529a4b35853ac4bf6f019f
Elizaveta <seldead@mail.ru>
2020-03-24 16:21:23 +0300

    Шаблон

---
+++ Lab/base/hash_table_on_lists.cpp
@@ -0,0 +1,19 @@
+#include "hash_table_on_lists.h"
+
+
+Data* hash_table_on_lists::Find(int key)
+{
+  return nullptr;
+}
+void hash_table_on_lists::Insert(Data data)
+{
+
+}
+void hash_table_on_lists::Delete(int key)
+{
+
+}
+void hash_table_on_lists::Print()
+{
+
+}
---
+++ Lab/base/ordered_line_table.cpp
@@ -0,0 +1,19 @@
+#include "ordered_line_table.h"
+
+
+Data* ordered_line_table::Find(int key)
+{
+  return nullptr;
+}
+void ordered_line_table::Insert(Data data)
+{
+
+}
+void ordered_line_table::Delete(int key)
+{
+
+}
+void ordered_line_table::Print()
+{
+
+}
