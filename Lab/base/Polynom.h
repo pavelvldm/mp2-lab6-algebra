5c1536f5cb50b9ada0cc07477ff2841383bf3778
Pavel <pavelvldm@outlook.com>
2020-03-30 22:21:27 +0300

    Переделана структура класса List, изменены методы, создан класс Monom, добавлены необходимые методы, переформатирован класс Polynom и его методы, добавлены тесты

--- Lab/base/Polynom.h
+++ Lab/base/Polynom.h
@@ -10,34 +10,92 @@
 using namespace std;
 
 const int MAX_POLYNOM_POW = 10;
+const int MAX_DEG = 999;
+
+class Monom
+{
+	double mul;
+	unsigned int deg;
+public:
+	Monom() : mul(0), deg(0)
+	{
+	}
+
+	Monom(const double& outMul, const unsigned int& outDeg) : mul(outMul), deg(outDeg)
+	{
+	}
+
+	~Monom()
+	{
+		mul = 0;
+		deg = 0;
+	}
+
+	double& GetMul() { return mul; }
+	unsigned int& GetDeg() { return deg; }
+	void ScalMul(const double& scal) { mul = scal * mul; }
+
+	Monom& operator=(const Monom& monomOut)
+	{
+		mul = monomOut.mul;
+		deg = monomOut.deg;
+
+		return *this;
+	}
+
+	Monom& operator+=(Monom& monomOut)
+	{
+		if (deg != monomOut.deg) throw 7;
+		mul += monomOut.mul;
+		return *this;
+	}
+
+	bool operator==(const Monom& monomOut) const
+	{
+		if ((mul == monomOut.mul) && (deg == monomOut.deg)) return true;
+		else return false;
+	}
+
+	bool operator!=(const Monom& monomOut) const
+	{
+		return !(*this == monomOut);
+	}
+
+	bool operator<(const Monom& monomOut) const
+	{
+		if (deg < monomOut.deg) return true;
+		else return false;
+	}
+};
 
 class Polynom
 {
-	List<double> coef;
+	List<Monom> Monoms;
 public:
 	Polynom();
-	Polynom(string &s);
-	Polynom(const Polynom &polyOut);
+	Polynom(string& s);
+	Polynom(const Polynom& polyOut);
 	~Polynom();
 
-	void CreatePolynom(string &s);
+	void CreatePolynom(string& s);
 	string CreateString();
 
-	Polynom& operator+=(Polynom &polyOut);
-	Polynom operator+(const Polynom &polyOut);
-	Polynom& operator*(const double &scal);
-	Polynom& operator*=(Polynom &polyOut);
-	Polynom operator*(Polynom &polyOut);
-	double ValuePoint(const double &x, const double &y, const double &z);
+	Polynom& operator+=(Polynom& polyOut);
+	Polynom operator+(const Polynom& polyOut);
+	Polynom& operator*(const double& scal);
+	Polynom& operator*=(Polynom& polyOut);
+	Polynom operator*(Polynom& polyOut);
+	double ValuePoint(const double& x, const double& y, const double& z);
 
-	Polynom& operator=(const Polynom &polyOut);
-	bool operator==(const Polynom &polyOut) const;
-	bool operator!=(const Polynom &polyOut) const;
+	Polynom& operator=(const Polynom& polyOut);
+	bool operator==(const Polynom& polyOut) const;
+	bool operator!=(const Polynom& polyOut) const;
 
 	void ClearZero();
+	Node<Monom>* FindDeg(const unsigned int& degOut);
 
-	friend istream& operator>>(istream &in, Polynom &polyOut);
-	friend ostream& operator<<(ostream &out, Polynom &polyOut);
+	friend istream& operator>>(istream& in, Polynom& polyOut);
+	friend ostream& operator<<(ostream& out, Polynom& polyOut);
 };
 
 #endif
--- Lab/base_test/test_polynom.cpp
+++ Lab/base_test/test_polynom.cpp
@@ -3,5 +3,163 @@
 
 TEST(Polynom, can_create_polynom)
 {
-	ADD_FAILURE();
+	string s = "5 x - 74 ,9 7y + 2, 75 z - 9 7 x^3y + 1, 9 9 9y^7x^3z - 11,7 zx^5 + 11";
+	ASSERT_NO_THROW(Polynom testP(s));
+}
+
+TEST(Polynom, can_create_copied_polynom)
+{
+	string s = "5 x - 74 ,9 7y + 2, 75 z - 9 7 x^3y + 1, 9 9 9y^7x^3z - 11,7 zx^5 + 11";
+	Polynom testP1(s);
+	Polynom testP2(testP1);
+
+	EXPECT_EQ(testP1, testP2);
+}
+
+TEST(Polynom, can_assign_polynoms)
+{
+	string s = "5 x - 74 ,9 7y + 2, 75 z - 9 7 x^3y + 1, 9 9 9y^7x^3z - 11,7 zx^5 + 11";
+	Polynom testP1(s);
+	Polynom testP2;
+
+	testP2 = testP1;
+	EXPECT_EQ(testP1, testP2);
+}
+
+TEST(Polynom, can_assign_polynom_to_itself)
+{
+	string s = "5 x - 74 ,9 7y + 2, 75 z - 9 7 x^3y + 1, 9 9 9y^7x^3z - 11,7 zx^5 + 11";
+	Polynom testP1(s);
+
+	ASSERT_NO_THROW(testP1 = testP1);
+}
+
+TEST(Polynom, can_transform_string_to_polynom)
+{
+	string s = "5 x - 74 ,9 7y + 2, 75 z - 9 7 x^3y + 1, 9 9 9y^7x^3z - 11,7 zx^5 + 11";
+
+	Polynom testP;
+	ASSERT_NO_THROW(testP.CreatePolynom(s));
+}
+
+TEST(Polynom, can_add_polynoms_1)
+{
+	string s1 = "3,75x^2 + y^2 - 7xz +x^2y + 1,5z^2y - 0,75";
+	string s2 = "-3,75x^2 + 7xz + x^2y + 1,5z^2y + 0,75";
+	string s3 = "y^2 +2x^2y + 3z^2y";
+
+	Polynom testP1(s1);
+	Polynom testP2(s2);
+	Polynom testResult(s3);
+
+	testP1 += testP2;
+	EXPECT_EQ(testResult, testP1);
+}
+
+TEST(Polynom, can_add_polynoms_2)
+{
+	string s1 = "3,75x^2 + y^2 - 7xz +x^2y + 1,5z^2y - 0,75";
+	string s2 = "-3,75x^2 + 7xz + x^2y + 1,5z^2y + 0,75";
+	string s3 = "y^2 +2x^2y + 3z^2y";
+
+	Polynom testP1(s1);
+	Polynom testP2(s2);
+	Polynom testResult(s3);
+
+	Polynom tR = testP1 + testP2;
+	EXPECT_EQ(testResult, tR);
+}
+
+TEST(Polynom, can_add_with_zero_polynom)
+{
+	string s1 = "3,75x^2 + y^2 - 7xz +x^2y + 1,5z^2y - 0,75";
+	string s2 = "";
+	string s3 = "3,75x^2 + y^2 - 7xz +x^2y + 1,5z^2y - 0,75";
+
+	Polynom testP1(s1);
+	Polynom testP2(s2);
+	Polynom testResult(s3);
+
+	testP1 += testP2;
+	EXPECT_EQ(testResult, testP1);
+}
+
+TEST(Polynom, can_multiply_polynoms)
+{
+	string s1 = "2xyz - 3x^2 + 7y^2z";
+	string s2 = "5x^2y^5 - 6z^7";
+	string s3 = "10x^3y^6z - 15x^4y^5 + 35x^2y^7z - 12xyz^8 + 18x^2z^7 - 42y^2z^8";
+
+	Polynom testP1(s1);
+	Polynom testP2(s2);
+
+	Polynom testResult(s3);
+	Polynom tR;
+	tR = testP1 * testP2;
+
+	EXPECT_EQ(testResult, tR);
+}
+
+TEST(Polynom, can_multiply_polynoms_2)
+{
+	string s1 = "2x^3yz - 4yzx^2";
+	string s2 = "5x^2y^3z + y^3zx^3";
+	string s3 = "6x^5y^4z^2 - 20x^4y^4z^2 + 2x^6y^4z^2";
+
+	Polynom testP1(s1);
+	Polynom testP2(s2);
+
+	Polynom testResult(s3);
+	testP1 *= testP2;
+
+	EXPECT_EQ(testResult, testP1);
+}
+
+TEST(Polynom, can_multiply_with_zero_polynom)
+{
+	string s1 = "2x^3yz - 4yzx^2";
+	string s2 = "";
+	string s3 = "";
+
+	Polynom testP1(s1);
+	Polynom testP2(s2);
+
+	Polynom testResult(s3);
+	testP1 *= testP2;
+
+	EXPECT_EQ(testResult, testP1);
+}
+
+TEST(Polynom, can_multiply_polynom_on_scalar)
+{
+	string s1 = "2x - 7 + 1,7z^5x + x^2y^5 - 0,6yz^4x^3";
+	string s2 = "4x - 14 + 3,4z^5x + 2x^2y^5 - 1,2yz^4x^3";
+
+	Polynom testP(s1);
+	Polynom testResult(s2);
+	testP = testP * 2;
+
+	EXPECT_EQ(testResult, testP);
+}
+
+TEST(Polynom, can_multiply_zero_polynom_on_scalar)
+{
+	string s1 = "";
+	string s2 = "";
+
+	Polynom testP(s1);
+	Polynom testResult(s2);
+	testP = testP * 2;
+
+	EXPECT_EQ(testResult, testP);
+}
+
+TEST(Polynom, can_compute_value_in_point)
+{
+	string s1 = "2x - 7,4 + 1,7z^5x + x^2y^3";
+	Polynom testP(s1);
+
+	double testResult = testP.ValuePoint(2, 3, 1);
+
+	EXPECT_EQ(108, testResult);
 }
